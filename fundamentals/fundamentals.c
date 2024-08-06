e818ddecc17a5b67b1f7e87c1691cc37dde5da13
Nancy Rana <nrana18@myseneca.ca>
2024-08-06 14:13:24 -0400

    V1

---
+++ fundamentals/fundamentals.c
@@ -0,0 +1,59 @@
+
+/* Version 1 */
+// Fundamental source module
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 80
+#define NUM_INPUT_SIZE 10
+#include "fundamentals.h"
+
+void fundamentals(void)
+{
+    // V1 //
+
+
+
+    printf("*** Start of Indexing Strings Demo ***\n");
+    char buffer1[BUFFER_SIZE];
+    char numInput[NUM_INPUT_SIZE];
+    size_t position;
+
+    // exiting the program with entering the 'q' string
+    do {
+        printf("Type not empty string (q - to quit):\n");
+        fgets(buffer1, BUFFER_SIZE, stdin);
+
+        buffer1[strlen(buffer1) - 1] = '\0';
+
+        if (strcmp(buffer1, "q") != 0)
+        {
+            printf("Type the character position within the string:\n");
+            fgets(numInput, NUM_INPUT_SIZE, stdin);
+            numInput[strlen(numInput) - 1] = '\0';
+            position = atoi(numInput);
+
+            // Check if the entered position equals the length of the typed string
+
+
+            if (position >= strlen(buffer1))
+            {
+                // Set the typed position to one less than the length of buffer1,
+                //  which matches the length of the user's typed string
+                position = strlen(buffer1) - 1;
+                printf("Too big... Position reduced to max. available\n");
+            }
+            printf("The character found at %d position is \'%c\'\n",
+                (int)position, buffer1[position]);
+        }
+    } while (strcmp(buffer1, "q") != 0);
+
+    // End of the program whenthe user types the 'q' string
+    printf("*** End of Indexing String Demo ***\n\n");
+
+
+    /* Version 2 */
+
+    /* Version 3 */
+
+
+
+}
