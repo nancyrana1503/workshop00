5669aa98043b573371a45db16d6de25507311158
Nancy Rana <nrana18@myseneca.ca>
2025-05-16 08:32:24 -0400

    Workshop 0 is ready to be submitted..

---
+++ oop345/Workshops/workshop00/w0.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+	    cout << "#### Workshop Zero ####" << endl;
+		cout << "Subject and Section:" << endl;
+	    cout << "Your subject and section goes here(example : AAA999 NXX)" << endl;
+		cout << "Name :" << endl;
+		cout << "Nancy Rana" << endl;
+		cout << "Student Number :" << endl;
+	    cout << "149951238" << endl;
+		cout << "Email :" << endl;
+	    cout << "nrana18@myseneca.ca" << endl;
+	return 0;
+}
