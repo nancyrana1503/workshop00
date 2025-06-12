16814e97cf0d1b408a5759c29381bc94805970d1
Nancy Rana <nancyrana155@gmail.com>
2025-06-12 09:45:37 -0400

    Update character.h
    Signed-off-by: Nancy Rana <nancyrana155@gmail.com>

--- asgn2/character.h
+++ asgn2/character.h
@@ -1,5 +1,11 @@
 #pragma once
 
+// Name: Nancy Rana
+// Seneca email: nrana18@myseneca.ca
+// Seneca Student ID: 149951238
+// Date: 12 June 2025
+// character.h
+
 #ifndef SENECA_CHARACTER_H
 #define SENECA_CHARACTER_H
 #include <iostream>
