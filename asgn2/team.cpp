2bf4465c269eb842209827346c7b09f7f285506b
Nancy Rana <nancyrana155@gmail.com>
2025-06-18 21:55:04 -0400

    Update team.cpp
    Signed-off-by: Nancy Rana <nancyrana155@gmail.com>

--- asgn2/team.cpp
+++ asgn2/team.cpp
@@ -1,57 +1,40 @@
-// Name: Nancy Rana
-// Seneca email: nrana18@myseneca.ca
-// Seneca Student ID: 149951238
-// Date: 12 June 2025
-// team.cpp
-
 #include "team.h"
 #include <iostream>
 
 namespace seneca {
 
-	/// Default constructor initializes an empty team
     Team::Team() : m_characters(nullptr), m_memberCount(0), m_name("") {}
 
-	/// Constructor initializes a team with a given name
     Team::Team(const char* name) : m_characters(nullptr), m_memberCount(0), m_name(name) {}
 
-	/// Copy constructor creates a deep copy of another team
-    Team::Team(const Team& other) {
-        *this = other;
-    }
-	/// Copy assignment operator creates a deep copy of another team
+    Team::Team(const Team& other) { *this = other; }
+
     Team& Team::operator=(const Team& other) {
-
+        
         if (this != &other) {
-            
-            clear();
 
+            clear();
             m_name = other.m_name;
             m_memberCount = other.m_memberCount;
-            m_characters = new Character * [m_memberCount];
 
+            m_characters = new Character * [m_memberCount];
             for (int i = 0; i < m_memberCount; ++i) {
-        
                 m_characters[i] = other.m_characters[i]->clone();
             }
         }
-        return *this;    }
+        return *this;
+    }
 
     Team::~Team() {
-        
         clear();
     }
 
     Team::Team(Team&& other) noexcept {
-     
         *this = std::move(other);
     }
 
-	/// Move assignment operator transfers ownership of resources from another team
     Team& Team::operator=(Team&& other) noexcept {
-        
         if (this != &other) {
-        
             clear();
 
             m_name = std::move(other.m_name);
@@ -61,63 +44,70 @@ namespace seneca {
             other.m_characters = nullptr;
             other.m_memberCount = 0;
         }
-        return *this;    }
+        return *this;
+    }
 
-	/// Helper function to clear the team members and free memory
     void Team::clear() {
-        
-        for (int i = 0; i < m_memberCount; ++i) {
-            
-            delete m_characters[i];
+        if (m_characters) {
+            for (int i = 0; i < m_memberCount; ++i) {
+                delete m_characters[i];
+            }
+            delete[] m_characters;
         }
-        delete[] m_characters;
-        
         m_characters = nullptr;
         m_memberCount = 0;
     }
 
     int Team::locate(const std::string& characterName) const {
-        
         for (int i = 0; i < m_memberCount; ++i) {
-        
             if (m_characters[i]->getName() == characterName) {
-            
                 return i;
             }
         }
-        return -1;    }
+        return -1;
+    }
 
     void Team::addMember(const Character* newChar) {
-        
-        if (locate(newChar->getName()) != -1)
-        
+        if (!newChar || locate(newChar->getName()) != -1)
             return;
 
         Character** temp = new Character * [m_memberCount + 1];
-        
         for (int i = 0; i < m_memberCount; ++i) {
-           temp[i] = m_characters[i];
+            temp[i] = m_characters[i];
         }
 
         temp[m_memberCount] = newChar->clone();
         delete[] m_characters;
-        
+
         m_characters = temp;
         ++m_memberCount;
     }
 
     void Team::removeMember(const std::string& name) {
-        
         int index = locate(name);
-        
         if (index == -1) return;
 
         delete m_characters[index];
-        
         for (int i = index; i < m_memberCount - 1; ++i) {
             m_characters[i] = m_characters[i + 1];
         }
         --m_memberCount;
+    }
+
+    Character* Team::operator[](size_t index) const {
+        return (index < static_cast<size_t>(m_memberCount)) ? m_characters[index] : nullptr;
+    }
+
+    void Team::showMembers() const {
+        if (m_name.empty() && m_memberCount == 0) {
+            std::cout << "No team." << std::endl;
+            return;
+        }
+
+        std::cout << "[Team] " << m_name << std::endl;
+        for (int i = 0; i < m_memberCount; ++i) {
+            std::cout << "    " << (i + 1) << ": " << *m_characters[i] << std::endl;
+        }
     }
 
 }
